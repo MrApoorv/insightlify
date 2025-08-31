29c2c9df25d3b8473a9c69adf690722563e88391
Utkarsh <utkarshapoorv2003@gmail.com>
2025-08-31 18:51:33 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
 from api.analyzer import router as analyzer_router
 from fastapi.middleware.cors import CORSMiddleware
+import os
 app = FastAPI()
+from dotenv import load_dotenv
 
+load_dotenv()
+PORT = int(os.getenv('PORT'))
+ENV = os.getenv('ENV')
 
 origins = [
         "http://localhost:3000",  
-        "https://yourdomain.com", 
+        "https://domain.com", 
     ]
 
 
 app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_credentials=True,  # Allow credentials (cookies, HTTP authentication)
-        allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-        allow_headers=["*"],     # Allow all headers
+        allow_credentials=True,  
+        allow_methods=["*"],     
+        allow_headers=["*"],     
     )
 
 @app.get("/")
 async def server():
     return {"Status": "Server Running"}
 
-# include analyzer routes
 app.include_router(analyzer_router, prefix="/analyzer", tags=["Analyzer"])
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
